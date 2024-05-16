a37f4a172b3d1a24ecbff9622b4e0dd05ec98179
zepultula <zepultula7@gmail.com>
2024-05-16 14:05:14 +0700

    เพิ่มอะไรบางอย่าง

---
+++ project_1/lib/screen/my_home_page.dart
@@ -0,0 +1,12 @@
+//? สำหรับส่งไปยัง main.dart
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello World!');
+  }
+}
